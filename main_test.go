18ce437f1bcb8b4072e21c843c1ef346dfc08c0c
Erik Lupander <erik.lupander@callistaenterprise.se>
2018-11-16 18:03:13 +0100

    Fixing test

--- main_test.go
+++ main_test.go
@@ -5,15 +5,25 @@ import "github.com/google/go-cmp/cmp"
 import . "github.com/smartystreets/goconvey/convey"
 
 func TestBDDStyle(t *testing.T) {
-	Convey("Given", t, func() {
+	Convey("Given three structs", t, func() {
 		t1 := T1{"VAL1", 12}
 		t2 := T1{"VAL2", 12}
+		t3 := T1{"VAL3", 1}
 
-		Convey("When", func() {
+		Convey("When t1 and t2 are compared", func() {
 			areEqual := cmp.Equal(t1, t2)
 
-			Convey("Then", func() {
+			Convey("Then they should be equal", func() {
 				So(areEqual, ShouldBeTrue)
+			})
+		})
+
+		Convey("When t1 and t3 is added and compared", func() {
+
+			areEqual := cmp.Equal(t1, t3)
+
+			Convey("Then they should not be equal", func() {
+				So(areEqual, ShouldBeFalse)
 			})
 		})
 	})
